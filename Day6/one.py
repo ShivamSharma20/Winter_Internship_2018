6d868af2f0c981d0f1e45a1efbeab115a758a433
ShivamSharma20 <cteshivam271@gmail.com>
2018-12-11 13:58:40 +0530

    successfull

---
+++ Day6/one.py
@@ -0,0 +1,13 @@
+"""Hi this is the module
+
+i can use it in another file
+
+Lets See what happened
+
+this is not comment"""
+
+
+if__name__=="__main__":
+print("This is the module")
+print("I dont want it to use as a script")
+print("lets see what happened")
---
+++ Day6/two.py
@@ -0,0 +1,7 @@
+import one
+
+print("this is the documentation of file",.__doc__)
+
+print("this is the name of the fie",.__name__)
+
+print("this is the name of the first file.",__name__)
